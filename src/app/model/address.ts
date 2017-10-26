6f588bf15a3c1ac59e155ccd2e2a89972f04e86b
lennieh <lh@lennieharvey.com>
2017-10-26 21:16:22 +0100

    couple of models

---
+++ src/app/model/address.ts
@@ -0,0 +1,9 @@
+export class Address {
+    id: number;
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
+    postTown: string;
+    postCode: string;
+    countryCode: string;
+}
